420c4528f622955e0676b0f4571a9a4946f7bcee
JNZhang <zjn811@gmail.com>
2021-02-27 23:43:18 -0500

    Main Window view model and command

---
+++ ManagementSystemForCourses/Model/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystemForCourses.Model
+{
+    public class UserModel : NotifyBase
+    {
+        private string _avatar;
+
+        public string Avatar
+        {
+            get { return _avatar; }
+            set { _avatar = value; this.DoNotify(); }
+        }
+
+        private string _username;
+
+        public string UserName
+        {
+            get { return _username; }
+            set { _username = value; this.DoNotify(); }
+        }
+
+        private int _gender;
+
+        public int Gender
+        {
+            get { return _gender; }
+            set { _gender = value; this.DoNotify(); }
+        }
+
+    }
+}
---
+++ ManagementSystemForCourses/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using ManagementSystemForCourses.Common;
+using ManagementSystemForCourses.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ManagementSystemForCourses.ViewModel
+{
+    public class MainViewModel : NotifyBase
+    {
+        public UserModel UserInfo { get; set; }
+
+        private int _searchText;
+
+        public int SearchText
+        {
+            get { return _searchText; }
+            set { _searchText = value; this.DoNotify(); }
+        }
+
+        private FrameworkElement _mainContent;
+
+        public FrameworkElement MainContent
+        {
+            get { return _mainContent; }
+            set { _mainContent = value; this.DoNotify(); }
+        }
+
+        public CommandBase NavChangedCommand { get; set; }
+
+        public MainViewModel() 
+        {
+            this.NavChangedCommand = new CommandBase();
+            this.NavChangedCommand.DoExecute = new Action<object>(DoNavChanged);
+            this.NavChangedCommand.DoCanExecute = new Func<object, bool>((o) => true);
+        }
+
+        private void DoNavChanged(object obj)
+        {
+
+        }
+
+
+    }
+}
