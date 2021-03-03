1ca9ae84983ee79be5e68f77b24ef2fe02f100fc
JNZhang <zjn811@gmail.com>
2021-03-03 14:53:05 -0500

    Dashboard: student checkin, Introduction, data monitor control(instument panel)

---
+++ ManagementSystemForCourses.Controls/InstrumentPanel.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManagementSystemForCourses.Controls
+{
+    /// <summary>
+    /// Interaction logic for InstrumentPanel.xaml
+    /// </summary>
+    public partial class InstrumentPanel : UserControl
+    {
+        public InstrumentPanel()
+        {
+            InitializeComponent();
+
+           this.SizeChanged += InstrumentPanel_SizeChanged;
+        }
+
+        private void InstrumentPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double minSize = Math.Min(this.RenderSize.Width, this.RenderSize.Height);
+            this.backEllipse.Width = minSize;
+            this.backEllipse.Height = minSize;
+        }
+
+    }
+}
